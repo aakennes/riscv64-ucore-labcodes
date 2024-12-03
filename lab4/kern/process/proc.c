a6b08f8f936c4ac68acea220c45bbdc2b838877b
Gadfly <133796091+FeliceRivarez@users.noreply.github.com>
2024-12-03 15:35:59 +0800

    lab4 EX2 complete

--- lab4/kern/process/proc.c
+++ lab4/kern/process/proc.c
@@ -332,12 +332,49 @@ int do_fork(uint32_t clone_flags, uintptr_t stack, struct trapframe *tf)
      */
 
     //    1. call alloc_proc to allocate a proc_struct
+    proc = alloc_proc();
+    if (proc == NULL)
+    {
+        goto fork_out;
+    }
+
     //    2. call setup_kstack to allocate a kernel stack for child process
+
+    /*
+    Znamya:
+        setup_kstack(): 就在本文件里，返回4的时候对应一个Error code:
+        #define E_NO_MEM            4   // Request failed due to memory shortage
+        返回0则说明setup成功。
+    */
+    if (setup_kstack(proc) != 0)
+    {
+        goto bad_fork_cleanup_proc;
+    }
+
     //    3. call copy_mm to dup OR share mm according clone_flag
+    /*
+    Znamya:
+        事实上不会返回error code
+        这里判定是否为0，在本project里只是出于仪式感，因为函数里面只有一个assert
+    */
+    if (copy_mm(clone_flags, proc) != 0)
+    {
+        goto bad_fork_cleanup_kstack;
+
     //    4. call copy_thread to setup tf & context in proc_struct
+    copy_thread(proc, stack, tf);
+
     //    5. insert proc_struct into hash_list && proc_list
+    proc->pid = get_pid();
+    hash_proc(proc);
+    list_add(&proc_list, &(proc->list_link));
+    nr_process ++;
+
     //    6. call wakeup_proc to make the new child process RUNNABLE
+    wakeup_proc(proc);
+
     //    7. set ret vaule using child proc's pid
+    return proc->pid;
 
 fork_out:
     return ret;
